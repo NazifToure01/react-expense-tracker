b2a267ac65ebfe9e3328660f99ec3a7c672a4823
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-10-16 13:03:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import { ExpenseInput } from "./components/ExpenseInput/ExpenseInput";
-import { ExpenseTotal } from "./components/ExpenseTotal/ExpenseTotal";
-import { IncomeInput } from "./components/IncomeInput/IncomeInput";
-import { List } from "./components/List/List";
-import { Logo } from "./components/Logo/Logo";
 import s from "./style.module.css";
 
 export function App() {
@@ -10,20 +5,20 @@ export function App() {
     <div className={s.main_container}>
       <div className={`row ${s.header}`}>
         <div className={`col-3`}>
-          <Logo title="ISpent" subtitle="Track your expenses !" />
+          Logo
         </div>
         <div className={`col-9 ${s.income_input}`}>
-          <IncomeInput />
+         Income input
         </div>
       </div>
       <div className={`row ${s.workspace}`}>
         <div className={`col-12  ${s.expense_input}`}>
-          <ExpenseInput />
+          Expense Input
         </div>
         <div className={`col-11 col-md-6 col-lg-4 ${s.expense_list}`}>
-          <List />
+         Expense History
           <div className={`col-12 ${s.expense_total}`}>
-            <ExpenseTotal />
+           Expense totals
           </div>
         </div>
       </div>
