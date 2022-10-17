ad4cda1f4c30ba06bd661384d5223cfca7e52e97
codiku-dev <codiku-dev@gmail.com>
2022-10-17 15:05:58 +0200

    step

--- src/components/ExpenseInput/ExpenseInput.jsx
+++ src/components/ExpenseInput/ExpenseInput.jsx
@@ -12,7 +12,12 @@ export function ExpenseInput(props) {
           />
         </div>
         <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
-          <input type="number" className="form-control" placeholder="Ex: 3.99" />
+          <input
+            type="number"
+            step="0.01"
+            className="form-control"
+            placeholder="Ex: 3.99"
+          />
         </div>
 
         <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
